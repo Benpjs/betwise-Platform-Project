68b10a08a392004737cfb145bcd2b44e4670893a
Benpjs <bernardpetersambo2017@gmail.com>
2025-06-10 16:09:20 +0100

    initial commit

--- services/authService.js
+++ services/authService.js
@@ -4,25 +4,25 @@ const User = require("../models/userModel");
 
 // REGISTER NEW USER
 
-// exports.registerUser = async ({ username, email, phoneNumber, password }) => {
-//   const existingUser = await User.findOne({ email });
-//   if (existingUser) throw new Error("User already exists");
+exports.registerUser = async ({ username, email, phoneNumber, password }) => {
+  const existingUser = await User.findOne({ email });
+  if (existingUser) throw new Error("User already exists");
 
-//   const hashedPassword = await bcrypt.hash(password, 10);
-//   const user = new User({ username, email, phoneNumber, password: hashedPassword });
-//   await user.save();
-//   return { msg: "User registered successfully" };
-// };
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, email, phoneNumber, password: hashedPassword });
+  await user.save();
+  return { msg: "User registered successfully" };
+};
 
-// // LOGIN AN EXISTING USER
+// LOGIN AN EXISTING USER
 
-// exports.loginUser = async ({ email, username, password }) => {
-//   const user = await User.findOne({ email, username });
-//   if (!user) throw new Error("Invalid credentials");
+exports.loginUser = async ({ email, username, password }) => {
+  const user = await User.findOne({ email, username });
+  if (!user) throw new Error("Invalid credentials");
 
-//   const isMatch = await bcrypt.compare(password, user.password);
-//   if (!isMatch) throw new Error("Invalid credentials");
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Invalid credentials");
 
-//   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
-//   return { token };
-// };
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  return { token };
+};
